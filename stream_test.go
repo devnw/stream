0057002b3eb1246f308aba1810e61d0ab4c9d4c1
Benji Vesterby <benji@devnw.com>
2021-12-22 15:28:08 -0500

    Adding more coverage for intercept

--- stream_test.go
+++ stream_test.go
@@ -4,6 +4,7 @@ import (
 	"constraints"
 	"context"
 	"testing"
+	"time"
 
 	. "go.structs.dev/gen"
 )
@@ -166,6 +167,7 @@ func InterceptTest[U ~[]T, T constraints.Signed](
 			defer cancel()
 
 			in := make(chan T)
+			defer close(in)
 
 			out := Intercept(ctx, in, func(v T) (T, bool) {
 				return v % 3, true
@@ -205,6 +207,101 @@ func Test_Intercept(t *testing.T) {
 	InterceptTest(t, "int8", IntTests[int8](100, 1000))
 	InterceptTest(t, "int32", IntTests[int32](100, 1000))
 	InterceptTest(t, "int64", IntTests[int64](100, 1000))
+}
+
+func Test_Intercept_NotOk(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	defer func() {
+		r := recover()
+		if r != nil {
+			t.Error("unexpected panic")
+		}
+	}()
+
+	in := make(chan int)
+	defer close(in)
+
+	go func() {
+		in <- 1
+		in <- 2
+		in <- 3
+		in <- 0
+	}()
+
+	out := Intercept(ctx, in, func(v int) (int, bool) {
+		if v == 0 {
+			return 0, true
+		}
+
+		return v, false
+	})
+
+	select {
+	case <-ctx.Done():
+		t.Fatal("context cancelled")
+	case out, ok := <-out:
+		if !ok {
+			t.Fatal("in closed prematurely")
+		}
+
+		if out != 0 {
+			t.Errorf("expected 0, got %v", out)
+		}
+	}
+}
+
+func Test_Intercept_ClosedChan(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	defer func() {
+		r := recover()
+		if r != nil {
+			t.Error("unexpected panic")
+		}
+	}()
+
+	in := make(chan int)
+
+	out := Intercept(ctx, in, func(v int) (int, bool) { return v, false })
+
+	close(in)
+
+	<-out
+}
+
+func Test_Intercept_Cancelled_On_Wait(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	defer func() {
+		r := recover()
+		if r != nil {
+			t.Error("unexpected panic")
+		}
+	}()
+
+	in := make(chan int)
+	defer close(in)
+
+	// Setup intercept
+	out := Intercept(ctx, in, func(v int) (int, bool) { return v, true })
+
+	// Push to in
+	in <- 1
+
+	// Wait for intercept routine to be scheduled
+	time.Sleep(time.Millisecond)
+
+	// Cancel the routine
+	cancel()
+
+	_, ok := <-out
+	if ok {
+		t.Error("expected closed channel")
+	}
 }
 
 // func (test *Test[any]) Run(t *testing.T) {
