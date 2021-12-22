333446e69356c6f9d92d5cd9867bf6193a1c0cf2
Benji Vesterby <benji@devnw.com>
2021-12-22 16:13:01 -0500

    Finished coverage for current functions

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module go.atomizer.io/stream
 
 go 1.18
 
-require go.structs.dev/gen v0.0.0-20211222120430-9fde850148d1
+require go.structs.dev/gen v0.0.0-20211222205854-0ab8b615977b
--- stream_test.go
+++ stream_test.go
@@ -304,181 +304,32 @@ func Test_Intercept_Cancelled_On_Wait(t *testing.T) {
 	}
 }
 
-// func (test *Test[any]) Run(t *testing.T) {
-// 	for name, test := range test.data {
-// 		t.Run(name, func(t *testing.T) {
-// 			t.Log("------------------------------------------------")
-// 			t.Log(test)
-// 			t.Log("------------------------------------------------")
-// 		})
-// 	}
-// }
+func Test_FanOut_Cancelled_On_Wait(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-// func Test_Int(t *testing.T) {
+	defer func() {
+		r := recover()
+		if r != nil {
+			t.Error("unexpected panic")
+		}
+	}()
 
-// 	// t.Log(IntTests[int8](100, 1000))
-// 	// t.Log(IntTests[uint8](100, 1000))
-// 	// t.Log(IntTests[int16](100, 1000))
-// 	// t.Log(IntTests[uint16](100, 1000))
-// 	// t.Log(IntTests[int32](100, 1000))
-// 	// t.Log(IntTests[uint32](100, 1000))
-// 	// t.Log(IntTests[int64](100, 1000))
-// 	// t.Log(IntTests[uint64](100, 1000))
-// 	// t.Log(FloatTests[float32](100, 1000))
-// 	// t.Log(FloatTests[float64](100, 1000))
+	in, out := make(chan int), make(chan int)
+	defer close(in)
+	defer close(out)
 
-// 	tests := Test[int8]{
-// 		data: map[string][][]int8{
-// 			"t1": IntTests[int8](100, 1000),
-// 		},
-// 	}
+	go func() {
 
-// 	tests.Run(t)
-// }
+		// Cancel the routine
+		defer cancel()
 
-// func Test_Pipe_Single(t *testing.T) {
-// 	ctx, cancel := context.WithCancel(context.Background())
-// 	defer cancel()
+		// Push to in
+		in <- 1
+		// Wait for intercept routine to be scheduled
+		time.Sleep(time.Millisecond)
+	}()
 
-// 	defer func() {
-// 		if r := recover(); r != nil {
-// 			t.Fatal(r)
-// 		}
-// 	}()
-
-// 	c1, c2 := make(chan int), make(chan int)
-
-// 	go Pipe(ctx, c1, c2)
-
-// 	go func() {
-// 		c1 <- 1
-// 	}()
-
-// 	out, ok := <-c2
-// 	if !ok {
-// 		t.Fatal("c2 closed")
-// 	}
-
-// 	if out != 1 {
-// 		t.Fatalf("expected 1, got %d", out)
-// 	}
-
-// 	close(c1)
-// 	timer := time.NewTimer(time.Second)
-
-// 	select {
-// 	case <-ctx.Done():
-// 		t.Fatal("context cancelled")
-// 	case <-timer.C:
-// 		t.Fatal("timeout")
-// 	case <-c2:
-// 	}
-// }
-
-// type testy struct {
-// 	name string
-// }
-
-// type testy2 io.ReadCloser
-
-// func c[T any](in ...T) chan T {
-// 	return make(chan T)
-// }
-
-// func Test_Forward_int(t *testing.T) {
-// 	testdata := map[string]struct {
-// 		data any
-// 	}{
-// 		"ints":    []int{1, 2, 3},
-// 	}
-
-// 	for name, test := range testdata {
-// 		t.Run(name, func(t *testing.T) {
-// 		})
-// 	}
-// }
-
-// func Test_Forward(t *testing.T) {
-// 	testdata := map[string]struct {
-// 		data any
-// 	}{
-// 		"ints":    []int{1, 2, 3},
-// 		"int32":   []int32{1, 2, 3},
-// 		"ints64":  []int64{1, 2, 3},
-// 		"bool":    []bool{true, false, true},
-// 		"string":  []string{"test1", "test2", "test3"},
-// 		"float32": []float32{1.99, 2.99, 3.99},
-// 		"float64": []float32{1.99, 2.99, 3.99},
-// 		"testy": []testy{
-// 			{"test1"},
-// 			{"test2"},
-// 			{"test3"},
-// 		},
-// 	}
-
-// 	for name, test := range testdata {
-// 		t.Run(name, func(t *testing.T) {
-// 			// ctx, cancel := context.WithCancel(context.Background())
-// 			// defer cancel()
-
-// 			// defer func() {
-// 			// 	if r := recover(); r != nil {
-// 			// 		t.Fatal(r)
-// 			// 	}
-// 			// }()
-
-// 			// c1, c2 := make(chan int), make(chan int)
-
-// 			// go Pipe(ctx, c1, c2)
-
-// 			// go func() {
-// 			// 	c1 <- 1
-// 			// }()
-
-// 			// out, ok := <-c2
-// 			// if !ok {
-// 			// 	t.Fatal("c2 closed")
-// 			// }
-
-// 			// if out != 1 {
-// 			// 	t.Fatalf("expected 1, got %d", out)
-// 			// }
-
-// 			// close(c1)
-// 			// timer := time.NewTimer(time.Second)
-
-// 			// select {
-// 			// case <-ctx.Done():
-// 			// 	t.Fatal("context cancelled")
-// 			// case <-timer.C:
-// 			// 	t.Fatal("timeout")
-// 			// case <-c2:
-// 			// }
-// 		})
-// 	}
-// }
-
-// func Test_FanOut(t *testing.T) {
-// 	testdata := map[string]struct {
-// 	}{
-// 		"": {},
-// 	}
-
-// 	for name, test := range testdata {
-// 		t.Run(name, func(t *testing.T) {
-
-// 		})
-// 	}
-// }
-// func Test_FanIn(t *testing.T) {
-// 	testdata := map[string]struct {
-// 	}{
-// 		"": {},
-// 	}
-
-// 	for name, test := range testdata {
-// 		t.Run(name, func(t *testing.T) {
-
-// 		})
-// 	}
-// }
+	// Setup intercept
+	FanOut(ctx, in, out)
+}
