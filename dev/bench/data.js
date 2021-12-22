c69dacd5601a7c4dd7b54889cfcd7fe6aab79a8e
github-action-benchmark <github@users.noreply.github.com>
2021-12-22 23:42:43 +0000

    add Benchmark Results (go) benchmark result for ec5d60ee3f89f0e0e45a4e15d38d601d7e918177

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1640211427938,
+  "lastUpdate": 1640216563629,
   "repoUrl": "https://github.com/devnw/stream",
   "entries": {
     "Benchmark Results": [
@@ -48,6 +48,54 @@ window.BENCHMARK_DATA = {
             "value": 884.4,
             "unit": "ns/op",
             "extra": "1361472 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "benji@devnw.com",
+            "name": "Benji Vesterby",
+            "username": "benjivesterby"
+          },
+          "committer": {
+            "email": "benji@devnw.com",
+            "name": "Benji Vesterby",
+            "username": "benjivesterby"
+          },
+          "distinct": true,
+          "id": "ec5d60ee3f89f0e0e45a4e15d38d601d7e918177",
+          "message": "Updating readme with docs and benchmark link",
+          "timestamp": "2021-12-22T18:41:54-05:00",
+          "tree_id": "32fc31d9ecae4c47aa6dc74841fffc49a82e05b1",
+          "url": "https://github.com/devnw/stream/commit/ec5d60ee3f89f0e0e45a4e15d38d601d7e918177"
+        },
+        "date": 1640216563048,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "Benchmark_Pipe",
+            "value": 565.4,
+            "unit": "ns/op",
+            "extra": "2114622 times\n2 procs"
+          },
+          {
+            "name": "Benchmark_Intercept",
+            "value": 549,
+            "unit": "ns/op",
+            "extra": "2155120 times\n2 procs"
+          },
+          {
+            "name": "Benchmark_FanIn",
+            "value": 1940,
+            "unit": "ns/op",
+            "extra": "611397 times\n2 procs"
+          },
+          {
+            "name": "Benchmark_FanOut",
+            "value": 1123,
+            "unit": "ns/op",
+            "extra": "1000000 times\n2 procs"
           }
         ]
       }
